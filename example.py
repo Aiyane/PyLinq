86f2fdcc3a029c5c3b70ac5c13b5fb5d886812be
zhangzhiqiang.aiyane <zhangzhiqiang.aiyane@bytedance.com>
2019-04-05 18:12:27 +0800

    增加example

---
+++ example.py
@@ -0,0 +1,20 @@
+from PyLinq.interpreter import run
+
+
+def main(sql_expr: str, data_sources: dict):
+    print(run(sql_expr, data_sources))
+
+
+if __name__ == '__main__':
+    test_sql_expr = "(SELECT Teacher.name FROM Teacher WHERE Teacher.year = 5)"
+    test_data = {
+        'Teacher': [
+            {'name': 'zhang', 'year': 5},
+            {'name': 'li', 'year': 3}
+        ],
+        'Student': [
+            {'number': 10, 'age': 18},
+            {'number': 20, 'age': 17}
+        ]
+    }
+    main(test_sql_expr, test_data)
